21f1a1ec46db7b2200eb973ad09018b18c378b9e
kim sangmin <sign0329@gmail.com>
2024-03-04 17:01:52 +0900

    setting

---
+++ src/main/java/com/ll/sb_2024_02_29/Sb20240229Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb_2024_02_29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb20240229Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb20240229Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sb_2024_02_29/Sb20240229ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb_2024_02_29;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb20240229ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
